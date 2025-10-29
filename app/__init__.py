b5717ccb4faded29edb83843a32cfe4afa742dd3
Mumina <muminaabdi13@gmail.com>
2025-10-29 16:22:55 -0400

    models, routes, migrations, and testing created

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
-from .routes.book_routes import books_bp
+from app.db import db, migrate
+from app.routes.book_routes import books_bp
+from app.models import book
+import os
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
-    # Register Blueprints here
-    app.register_blueprint(books_bp)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+    if config:
+        # Merge `config` into the app's configuration
+        # to override the app's default settings
+        app.config.update(config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(books_bp)
     return app
